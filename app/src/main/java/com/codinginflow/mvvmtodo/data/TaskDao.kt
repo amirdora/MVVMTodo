8aa30091293ec904108fdc4b86686975ab7ad2ec
Amir Dora <amirdora@campus.uni-paderborn.de>
2021-10-05 15:39:18 +0200

    combining multiple flows

--- app/src/main/java/com/codinginflow/mvvmtodo/data/TaskDao.kt
+++ app/src/main/java/com/codinginflow/mvvmtodo/data/TaskDao.kt
@@ -1,13 +1,26 @@
 package com.codinginflow.mvvmtodo.data
 
 import androidx.room.*
+import com.codinginflow.mvvmtodo.ui.tasks.SortOrder
 import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface TaskDao {
 
-    @Query("Select * FROM TASK_TABLE")
-    fun getAllTasks() : Flow<List<Task>>
+    fun getTasks(query: String, sortOrder: SortOrder, hideCompleted: Boolean): Flow<List<Task>> =
+        when (sortOrder) {
+            SortOrder.BY_DATE -> getAllTasksSortedByDateCreated(query, hideCompleted)
+            SortOrder.BY_NAME -> getAllTasksSortedByName(query, hideCompleted)
+        }
+
+    @Query("Select * FROM TASK_TABLE WHERE(completed != :hideCompleted OR completed = 0) AND name LIKE '%' || :searchQuery || '%' ORDER BY important DESC, name")
+    fun getAllTasksSortedByName(searchQuery: String, hideCompleted: Boolean): Flow<List<Task>>
+
+    @Query("Select * FROM TASK_TABLE WHERE(completed != :hideCompleted OR completed = 0) AND name LIKE '%' || :searchQuery || '%' ORDER BY important DESC, created")
+    fun getAllTasksSortedByDateCreated(
+        searchQuery: String,
+        hideCompleted: Boolean
+    ): Flow<List<Task>>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insert(task: Task)
--- app/src/main/java/com/codinginflow/mvvmtodo/ui/tasks/TasksFragment.kt
+++ app/src/main/java/com/codinginflow/mvvmtodo/ui/tasks/TasksFragment.kt
@@ -1,12 +1,17 @@
 package com.codinginflow.mvvmtodo.ui.tasks
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
+import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.codinginflow.mvvmtodo.R
 import com.codinginflow.mvvmtodo.databinding.FragmentTasksBinding
+import com.codinginflow.mvvmtodo.util.onQueryTextChanged
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -17,6 +22,8 @@ class TasksFragment : Fragment(R.layout.fragment_tasks) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        setHasOptionsMenu(true)
 
         val binding = FragmentTasksBinding.bind(view)
 
@@ -34,4 +41,40 @@ class TasksFragment : Fragment(R.layout.fragment_tasks) {
             tasksAdapter.submitList(it)
         }
     }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.menu_fragment_tasks, menu)
+
+        val searchItem = menu.findItem(R.id.action_search)
+        val searchView = searchItem.actionView as SearchView
+
+        searchView.onQueryTextChanged {
+            viewModel.searchQuery.value = it
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_sort_by_name -> {
+                viewModel.sortOrder.value = SortOrder.BY_NAME
+                true
+            }
+            R.id.action_sort_by_date -> {
+                viewModel.sortOrder.value = SortOrder.BY_DATE
+                true
+            }
+            R.id.action_hide_completed_tasks -> {
+                item.isChecked = !item.isChecked
+                viewModel.hideCompleted.value = !item.isChecked
+
+                true
+            }
+            R.id.action_delete_all_completed_tasks -> {
+
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
 }
+
